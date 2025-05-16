943c6da2044e0851e2cdbceaaffa46ad0b7daff4
subbu <subbunandyala48@gmail.com>
2025-05-16 15:32:16 +0530

    hello world

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 PERSON1=Trump
 PERSON2=Musk
 
-echo "$PERSON1:: Hey Musk , How are you?"
-echo "Musk:: Hello $PERSON1 I am fine . what's up"
+echo "$PERSON1:: Hey $PERSON2 , How are you?"
+echo "$PERSON2:: Hello $PERSON1 I am fine . what's up"
 echo "$PERSON1: I am fine too. How are you doing?"
-echo "Musk:: Nothing, just going o mars. will u come."
+echo "$PERSON2:: Nothing, just going o mars. will u come."
 echo "$PERSON1:: Sorry,you carry on! once you come back I will go"
