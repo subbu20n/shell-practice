5a046cb1c78de67d9ee15efa616c83ea95dbc8ca
subbu <subbunandyala48@gmail.com>
2025-05-16 15:30:24 +0530

    hello world

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey Musk , How are you?"
+echo "Musk:: Hello $PERSON1 I am fine . what's up"
+echo "$PERSON1: I am fine too. How are you doing?"
+echo "Musk:: Nothing, just going o mars. will u come."
+echo "$PERSON1:: Sorry,you carry on! once you come back I will go"
