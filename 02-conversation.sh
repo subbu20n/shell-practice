68aee945f6f7d47ee45ac06015e3bb58abe205a5
subbu20n <subbunandyala48@gmail.com>
2025-05-16 11:25:03 +0530

    Delete 02-conversation.sh

--- 02-conversation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Trump:: Hey Musk, How are you?"
-echo "Musk:: Hello Trump, I am fine. How are you doing"
-echo "Trump: I am fine too. what's up?"
-echo "Musk:: Nothing, just going to mars now, will you come"
-echo "Trump::   Sorry,you carry on! I will come once you come back"
