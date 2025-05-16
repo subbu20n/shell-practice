79e0c8e4264cbfe9c59789f1057b87839ccd595e
subbu20n <subbunandyala48@gmail.com>
2025-05-16 16:59:14 +0530

    Delete 02-conversations.sh

--- 02-conversations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Subbu:: Hlo Arun, I am going to kadapa"
-echo " Arun:: HI Subbu, ok ok caryy on"
-echo "Subbu:: If u free will u come?"
-echo "Arun:: No, I have work, you go"
-echo "Subbu:: ok Arun"  
