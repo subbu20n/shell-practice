55950f01f1075628af00c122198ee210938b9728
subbu <subbunandyala48@gmail.com>
2025-05-16 16:20:37 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,10 @@
 #!/bin bash
 
-PERSON1=Subbu
+PERSON1=Supriya
 PERSON2=Arun
 
 echo "$PERSON1:: Hlo $PERSON2, I am going to kadapa"
 echo " $PERSON2:: HI $PERSON1, ok ok caryy on"
 echo "$PERSON1:: If u free will u come?"
 echo "$PERSON2:: No, I have work, you go"
-echo "$PERSON1:: ok $PERSON2"
+echo "$PERSON1:: ok $PERSON2"s
