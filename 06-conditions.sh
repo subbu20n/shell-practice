b581aceacd336a814cef28418cd719c302ad8949
subbu <subbunandyala48@gmail.com>
2025-05-17 15:18:35 +0530

    special vars

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ]
+if [ $NUMBER -lt 199 ]
 then
- echo "given number $NUMBER is less than 10"
-else
- echo "given number $NUMBER is not less tham 10"
-fi
+   echo "given number $NUMBER is less than 10"
+else 
+   echo "given number $NUMBER is not less than 10"
+if 
