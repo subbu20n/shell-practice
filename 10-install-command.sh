f32e8fa30c05ac4c7d39ff253db618e5e3adb902
subbu <subbunandyala48@gmail.com>
2025-05-17 12:43:47 +0530

    special vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,9 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR:: please run this script with root access"
-   exit 1
+   exit 1 #give other than 0 upto 127
 else 
    echo "you are running this root access"
 fi 
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then 
+   echo "installing mysql is ... SUCCESS"
+else 
+   echo "installing mysql is ...FAILURE"
+   #exit 1
+fi
