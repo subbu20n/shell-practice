9ae7852d745ad2bc3a05b72db63aa9cab2c60a81
subbu <subbunandyala48@gmail.com>
2025-05-17 15:05:23 +0530

    special vars

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "all variables passed to the script"
-echo "number of variables passed"
-echo "script name"
-echo "present woorking directory"
-echo "home directory of the user of the script"
-echo "which user running the script"
-echo "pid of the current script"
+echo "all variables passed to the script: $@"
+echo "number of variables passed: $#"
+echo "script name:$0"
+echo "present woorking directory: $PWD"
+echo "home directory of the user of the script: $HOME"
+echo "which user running the script: $USER"
+echo "pid of the current script: $$"
 sleep 10 &
-echo "pid of the last command in background"
-echo "exit status of previous command"
+echo "pid of the last command in background: $!"
+echo "exit status of previous command: $?"
