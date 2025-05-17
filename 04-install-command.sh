9954df995993c1005e97aa786d81290c003f47a6
subbu <subbunandyala48@gmail.com>
2025-05-17 17:45:17 +0530

    special vars

--- 04-install-command.sh
+++ 04-install-command.sh
@@ -6,5 +6,5 @@ if [ $NUMBER -it 0 ]
 then
     echo "given number $NUMBER is less than 10"
 else 
-    echo "given number $NUMBER is not less than 1o"
+    echo "given number $NUMBER is not less than 10"
 fi    
