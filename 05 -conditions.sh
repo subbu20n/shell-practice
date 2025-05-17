77c15ef2899b96a417f53dcee2d54d59a2910394
subbu <subbunandyala48@gmail.com>
2025-05-17 14:57:36 +0530

    special vars

---
+++ 05 -conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "all variables passed to the script"
+echo "number of variables passed"
+echo "script name"
+echo "present woorking directory"
+echo "home directory of the user of the script"
+echo "which user running the script"
+echo "pid of the current script"
+sleep 10 &
+echo "pid of the last command in background"
+echo "exit status of previous command"
--- 05-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "enter your pin: "
-
-read  -s PIN
-
-echo "your number is:: "
