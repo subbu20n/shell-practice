e3e0727d9dec34ad552c6e6cecf525deaab81994
subbu <subbunandyala48@gmail.com>
2025-05-17 21:01:37 +0530

    special vars

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=SUBBU
+PERSON2=SUPRIYA
+
+echo "$PERSON1:hi $PERSON2, i love u, im getting fear to tell you"
+echo "$PERSON2:hlo $PERSON1, im waiting for this message till to you"
+echo "$PERSON1:ill text you this im going to offlinre to 2 days"
+echo "$PERSON2: why reyy i want to meet you directly when you free"
+echo "$PERSON1:no im always busy right now im npt able to meet you"
