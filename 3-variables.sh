21c48e71ed7d727554c21d6e9d996307ffcd820d
subbu <subbunandyala48@gmail.com>
2025-05-17 21:45:01 +0530

    special vars

--- 3-variables.sh
+++ 3-variables.sh
@@ -4,7 +4,7 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON2: hey arun come to room"
-echo "Arun:  hlo $PERSON2 i have work"
+echo "$PERSON1:  hlo $PERSON2 i have work"
 echo "$PERSON2: why man come ntg will happen"
-echo "Arun: ill try,ok"
+echo "$PERSON1: ill try,ok"
 echo "$PERSON2: ok ok!"
