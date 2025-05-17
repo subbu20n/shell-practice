6a5e65a71dec0ab7b1e2cba08d066fda63598d4e
subbu <subbunandyala48@gmail.com>
2025-05-17 10:40:08 +0530

    helo world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Subbu
+PERSON2=Arun
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hlo $PERSON1, i am fine. what about you"
+echo "$PERSON1:: I am fine too. what is your plam tmrw"
+echo "$PERSON2:: Nothing, go to office"
+echo "$PERSON1:: ohh! ok carry on"
