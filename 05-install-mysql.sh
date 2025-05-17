8b55c68369e40c1ec40d5c27331036c33eabe817
subbu <subbunandyala48@gmail.com>
2025-05-17 17:54:38 +0530

    special vars

--- 05-install-mysql.sh
+++ 05-install-mysql.sh
@@ -5,8 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access"
+    exit 1 # give other number 0 upto 127
 else
     echo "you are running with root access" 
 fi
 
-dnf install mysql -y
+dnf install mysql -y 
