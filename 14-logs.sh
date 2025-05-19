69cd0140a266bd757b0bc7cd3367a8482b13bb26
subbu <subbunandyala48@gmail.com>
2025-05-19 21:07:04 +0530

    colours

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript.logs/"
+LOGS_FOLDER="/var/log/shellscript-logs/"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
