6a7b8b27174f3d53b5423106f738be735a484804
subbu <subbunandyala48@gmail.com>
2025-05-22 18:24:39 +0530

    hello world started

---
+++ script-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GREETING="Hello,Good Morning"
+
+echo "$GREETING"
+echo "PID of SCRIPT-1 : $$"
+./script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "PID of SCRIPT-2: $$"
+
+echo "$GREETING"
