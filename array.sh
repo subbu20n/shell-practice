031bee4f8bbab8d20ff0bc54c5fa2a4a0e19de85
subbu <subbunandyala48@gmail.com>
2025-05-23 21:03:32 +0530

    hello world started

--- array.sh
+++ array.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-MOVIES=("patan" "suri" "poori" "seeta")
+# MOVIES=("patan" "suri" "poori" "seeta")
 
-echo "first movie: ${MOVIES[0]}"
+# echo "first movie: ${MOVIES[0]}"
 
-echo "first movie: ${MOVIES[2]}"
+# echo "first movie: ${MOVIES[2]}"
 
-echo "first movie: ${MOVIES[7]}"
+# echo "first movie: ${MOVIES[7]}"
 
-echo "first movie: ${MOVIES[@]}"
+# echo "first movie: ${MOVIES[@]}"
+
+subbu=("i love supriya" "sindu i want subbu" " sooraj i want siva")
+
+echo "print subbu: ${subbu[3]}"
