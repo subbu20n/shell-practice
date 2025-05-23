f79be600000ae2ae70787cc1dd879e9d6ff38d85
subbu <subbunandyala48@gmail.com>
2025-05-23 20:20:33 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Supriya
+PERSON2=Subbu
+
+echo "$PERSON1:: hi $PERSON2, how are you"
+echo "$PERSON2:: hlo $PERSON1 ,im fine too and you"
+echo " $PERSON1:: i want to sau something foer u"
+echo "$PERSON2:hlo im fine ok"
+echo "ok ok ill tell you later"
