dabf7a293d53bd658526f7a2673b52ea64a59e27
subbu <subbunandyala48@gmail.com>
2025-05-23 20:12:02 +0530

    hello world started

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Subbu:: hi ARUN, how are you"
+echo "Arun:: hlo subbu, im am fine. what about you?"
+echo "SUBBU::Ime too. whats sup?"
+echo "Arun:: iam going to bir will you come"
+echo "Subbu:: no you carrry on!"
