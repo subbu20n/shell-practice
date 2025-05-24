178d697540f611e20c1b5851a724556eb18e71f8
subbu <subbunandyala48@gmail.com>
2025-05-24 06:22:57 +0530

    hello world started

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 USERID=$(id -u) 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR :: please run this script with root access"
+    echo "$R ERROR :: please run this script with root access $N"
     exit 1
 else 
     echo "run this script with root access"
@@ -11,9 +15,9 @@ fi
 VALIDATE(){
     if [ $1 -eq 0 ]
 then
-    echo "Installing $2 is .. success"
+    echo "Installing $2 is ..  $G success $N"
 else 
-    echo "installing $2 is .. failure"
+    echo "installing $2 is .. $R failure  $N"
     exit 1
 fi        
 }
@@ -25,7 +29,7 @@ then
     dnf install mysql -y
     validate $? "MYSQL"
 else 
-    echo "mysql is already installed ... nothing to do"
+    echo "nothing to do mysql ... $Y already installed $N"
 fi    
 
 dnf list installed nginx
@@ -35,7 +39,7 @@ then
     dnf install mysql -y
     validate $? "nginx"
 else 
-    echo "nginx is already installed ... nothing to do"
+    echo "nothing to do nginx ... $Y already $N"
 fi    
 
 dnf list installed python3
@@ -45,6 +49,6 @@ then
     dnf install mysql -y
     validate $? "python3"
 else 
-    echo "python3 is already installed ... nothing to do"
+    echo "nothing to do python3 ... $Y already installed $N"
 fi    
 
