01ddd056b528c63cbe1de818f1d5eb8ffa228d11
subbu <subbunandyala48@gmail.com>
2025-05-25 20:58:57 +0530

    hello world started

--- 17-sets.sh
+++ 17-sets.sh
@@ -69,4 +69,4 @@ systemctl start user
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_
