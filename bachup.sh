4da514c463478e6e230852f5ede50c8639066d3f
subbu <subbunandyala48@gmail.com>
2025-05-26 12:20:33 +0530

    hello world started

--- bachup.sh
+++ bachup.sh
@@ -81,3 +81,7 @@ DAYS=$(3:-14)
         echo -e "no log files found older than $days ... $Y Skipping $N"
      fi                   
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE 
