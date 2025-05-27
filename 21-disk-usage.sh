acd665fc104a0aecec837a535782b19994ac57de
subbu <subbunandyala48@gmail.com>
2025-05-27 14:13:37 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -3,17 +3,16 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1
 MSG=""
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+#IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do 
   USAGE=$(echo $line | awk '{print $6f}' |cut -d "%" -f1)
   PARTITION=$(echo $line | awk '{print $7f}')
 
-  if [ $USAGE -ge $DISK_THRESHOLD ]
-  then  
-      MSG+="Hard Disk Usage on $PARTITION: $USAGE <br>"
-  fi    
-done <<< $DISK_USAGE       
-
-echo $MSG
+#   if [ $USAGE -ge $DISK_THRESHOLD ]
+#   then  
+#       MSG+="Hard Disk Usage on $PARTITION: $USAGE <br>"
+#   fi    
+done <<< $DISK_USAGE      
+#echo $MSG
