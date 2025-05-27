e510adeacbd01eb5636533250e2636c24e2e9345
subbu <subbunandyala48@gmail.com>
2025-05-27 11:40:57 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -3,8 +3,9 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in projects it will be 75%
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS= read line 
+while IFS= read -r line 
 do 
   USAGE=$(echo $line | awk '{print $6f}'| cut -d "%" -f1)
   PARTITION=$(echo $line | awk '{print $7f}')
