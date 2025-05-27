133e5456c7b14c3e76a85ef7db702995674ff26d
subbu <subbunandyala48@gmail.com>
2025-05-27 11:10:54 +0530

    hello world started

---
+++ 21-disk-usage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # in projects it will be 75%
+
+
+while IFS= read -r line 
+do 
+  USAGE=$(echo $line | awk '{print $6f}'| cut -d "." -f1)
+  PARTITION=$(echo $line | awk '{print $6f}')
+
+  echo "$PARTITION: $USAGE"
+  done <<< $DISK_USAGE
