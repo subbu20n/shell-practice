f4c35ddfe9a8c3187b5d7c72a53dc382e6892910
subbu <subbunandyala48@gmail.com>
2025-05-27 22:31:23 +0530

    hello world started

--- backup.sh
+++ backup.sh
@@ -54,7 +54,8 @@ then
     exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime $days)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 if [ ! -z "$FILES" ]
 then 
-    echo "files to zip are: $FILES"
+    echo "Files to zip are: $FILES"
+fi    
