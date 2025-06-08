93ed7f79f0cca21bbde80fdc37ed87d2e84b73d9
subbu <subbunandyala48@gmail.com>
2025-06-08 09:49:15 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS= read line
+while IFS= read line 
 do 
-  USAGE=$(echo $line | awk '{print $6f}' |cut -d "%" -f1)
-  PARTITION=$(echo $line | awk '{print $7f}')
+  USAGE=$( echo $line | awk '{print $6f}' | cut -d "%" -f1)
+  PARTITION=$( echo $line | awk '{print $7f}')
 
-   if [ $USAGE -ge $DISK_THRESHOLD ]
-   then  
-       MSG+="Hard Disk Usage on $PARTITION: $USAGE % <br>" # <br> represents html new
-   fi    
-done <<< $DISK_USAGE      
-    #echo -e $MSG
-
-  sh mail.sh "Devops Team" "High Disk Usage" "$IP" "$MSG" "subbunandyala68@gmail.com" "ALERT-High Disk Usage"
+  if [ $USAGE -ge $DISK_THRESHOLD ]
+  then
+      MSG+=HIGH DISK USAGE ON $PARTITION: $USAGE % <br> 
+  fi 
+done <<< $DISK_USAGE 
+# echo -e $MSG
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "subbunandyala68@gmail.com" "ALERT-High Disk Usage"       
