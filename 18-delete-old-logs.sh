ca47c404af350ade28348ed33eadeb910ebee80e
subbu <subbunandyala48@gmail.com>
2025-06-22 09:16:56 +0530

    shell-practice

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -33,7 +33,7 @@ VALIDATE(){
 
 echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE=$(find  -name "*.log" -mtime +14)
 
 while IFS= read -r filepath
 do
@@ -41,4 +41,4 @@ do
     rm -rf $filepath
 done <<< $FILES_TO_DELETE
 
-echo "Script executed successfully"
+echo "Script executed successfully" 
