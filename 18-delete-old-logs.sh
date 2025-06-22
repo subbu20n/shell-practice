b6aa0c7abf872fdd53f87eda48717bc1edaf1360
subbu <subbunandyala48@gmail.com>
2025-06-22 09:15:15 +0530

    shell-practice

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,48 +1,44 @@
-#!/bin/bash 
-START_TIME=$(date +%s)
-USERID=$(id -u)
+#!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SOURCE_DIR="/home/ec2-user/app-logs"
+SOURCE_DIR=/home/ec2-user/app-logs
 
-mkdir -p /app 
-echo "script execution started at: $(date)" | tee -a $LOG_FILE 
+mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]
-then 
-   echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE 
-   exit 1 
-else 
-   echo "you are running with root access" | tee -a $LOG_FILE 
-fi 
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
 
-# validate function takes input as exit status what command they tried to install 
-VALIDATE() {
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
     if [ $1 -eq 0 ]
-    then 
-       echo -e "installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE 
-    else 
-       echo -e "instaling  $2 is... $R FAILURE $N" | tee -a $LOG_FILE 
-       exit 1 
-    fi       
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
-FILES_TO_DELETE=$(find -name "*.log" -mtime +14)
-while IFS= read -r filepath 
-do 
-   echo "deleting file: $filepath"
-   rm -rf $filepath 
-done >>>$FILES_TO_DELETE
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
-echo -e "script execution completed successfully, $Y timetaken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE 
-       
+while IFS= read -r filepath
+do
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+done <<< $FILES_TO_DELETE
+
+echo "Script executed successfully"
