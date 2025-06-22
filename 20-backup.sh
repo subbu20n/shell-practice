93353b74fd660cc7577a5c5d5aecabe842aaddab
subbu <subbunandyala48@gmail.com>
2025-06-22 09:37:22 +0530

    shell-practice

--- 20-backup.sh
+++ 20-backup.sh
@@ -1,90 +1,61 @@
-#!/bin/bash
+#!/bin/bash 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+DAYS=${3:-14}
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/backup.log"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE=$LOGS_FOLDER/backup.log 
 
+mkdir -p /app 
+echo "script started execution at: $(date)" | tee -a $LOG_FILE 
 
-mkdir -p $LOGS_FOLDER
-echo "Script started execution at : $(date)" | tee -a $LOG_FILE
-check_root(){
 if [ $USERID -ne 0 ]
 then 
-   echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 # give other than 0 upto 127
-else
-   echo "you are running with root access" | tee -a $LOG_FILE
+   echo -e "$R ERROR:: please run this script with root acces $N" | tee -a $LOG_FILE 
+   exit 1 
+else 
+   echo " yor are running with root access" | tee -a $LOG_FILE 
 fi 
-}
 
-#validate function takes input as exit status what command they tried to install
-
+# validate function takes input as exit status what command tried to install 
 VALIDATE(){
    if [ $1 -eq 0 ]
    then 
-      echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE 
-   else
-      echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-      exit 1
-   fi      
+      echo -e "installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE 
+   else 
+      echo -e "installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE 
+   fi       
 }
 
-usage(){
-   echo -e "$R USAGE:: $N sh 20-backup.sh <source directory> <destination directory> <days(opyional)>"
+USAGE(){
+   echo "$R USAGE:: $N sh 20-backup.sh <source-directory> <destination-directory> <optional(days)>"
    exit 1
 }
 
 if [ $# -lt 2 ]
 then 
-    usage
-fi
+    USAGE
+fi 
 
 if [ ! -d $SOURCE_DIR ]
 then 
-   echo -e "$R source directory $SOURCE_DIR does not exist please check $N"
+   echo -e "source directory $SOURCE_DIR does not exist please check $N" | tee -a $LOG_FILE 
    exit 1
-fi
+fi 
 
 if [ ! -d $DEST_DIR ]
 then 
-   echo -e "$R destination directory $DEST_DIR does not exist please check $N"
-   exit 1
-fi
+   echo -e "destination directory $DEST_DIR does not exist please check $N" | tee -a $LOG_FILE 
+   exit 1 
+fi 
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-
-if [ ! -z "$FILES" ]
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +$days)
+if [ ! -z $FILES ]
 then 
-    echo "files to zip are: $FILES" 
-
-     TIMESTAMP=$(date +%F-%H-%M-%S)
-    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
-
-if [ -f "$ZIP_FILE" ]
-then
-    echo "successfully created  zip file"
-while IFS= read -r filepath
-do
-    echo "Deleting file: $filepath"
-    rm -rf $filepath
- done <<< $FILES 
- 
-     echo -e "log file found older than $days from source directory removed ... $G SUCCESSFULLY $N"
-else
-     echo -e "zip file creation failure ... $R FAILURE $N"
-fi     
-        
-else
-    echo -e "no log file found older than $days ... $Y SKIPPING $N"
-fi     
-
+   echo "files found" 
+else 
+   echo -e "no log files found older than $days ... $Y SKIPPING $N" | tee -a $LOG_FILE 
+fi       
