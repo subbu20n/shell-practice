eb91f99201acf83061d729c2637e2110404ff995
subbu <subbunandyala48@gmail.com>
2025-06-22 09:55:42 +0530

    shell-practice

--- 20-backup.sh
+++ 20-backup.sh
@@ -56,6 +56,27 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 if [ ! -z $FILES ]
 then 
    echo "files to zip are: $FILES"
+
+   TIMESTAMP=$(date +%f-%m-%h-%s)
+   ZIP_FILE=$DEST_DIR/app-logs-$TIMESTAMP.zip 
+   find $SOURCE_DIR -name "*.log" -mtime +14 | zip -@ $ZIP_FILE 
+   if [ -f $ZIP_FILE ]
+   then 
+      echo "successfully created a zip file" 
+   while IFS= read filepath 
+   do 
+     echo -e "Deleting filepath: $filepath"
+     rm -rf $filepath 
+   done >>> $FILES_TO_DELETE 
+      echo -e "log files older than 14days from source directory removed ... $G SUCCESS $N" | tee -a $LOG_FILE 
+   else 
+      echo -e "zip file creation failure" | tee -a $LOG_FILE 
+   fi       
 else 
    echo -e "no log files found older than 14days ... $Y SKIPPING $N" | tee -a $LOG_FILE 
 fi       
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "script execution completed successfully, $Y timetaken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE 
