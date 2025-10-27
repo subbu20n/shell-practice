ea8e8894986448637fb849646ea0f32dc6d446a1
subbu <subbunandyala48@gmail.com>
2025-10-27 08:31:56 +0530

    hello world

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-PERSON1 = Subbu 
-PERSON2 = Arun 
+PERSON1 = SUBBU
+PERSON2 = ARUN 
 
 echo "$PERSON1: hi $PERSON2, how are you" 
 echo "$PERSON2: Hlo $PERSON1, i am fine, what about you?" 
 echo "$PERSON1: Iam fine $PERSON2. where are you"
 echo "$PERSON2: i am in bangalore $PERSON1" 
-echo "$PERSON1" ok $PERSON2!"
+echo "$PERSON1: ok $PERSON2"
