5b5afa1d6977982b64a1df721f05d0bf21937806
subbu <subbunandyala48@gmail.com>
2025-10-27 08:20:08 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "Subbu: Hi , Arun how are you"
+echo "Arun: Hlo Subbu, I am fine, what about you"
+echo "Subbu: I am fine. where are you?"
+echo "Arun: I am in bangalore"
+echo "Subbu: Ok Arun!" 
