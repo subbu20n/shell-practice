08f19cd6d3681d2e99070bd95db79326e9049199
subbu <subbunandyala48@gmail.com>
2025-10-27 08:43:49 +0530

    hello world

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi $PERSON2, how are you" 
+echo "$PERSON2:: Hlo $PERSON1, i am fine, what about you?" 
+echo "$PERSON1:: Iam fine $PERSON2. where are you"
+echo "$PERSON2:: i am in bangalore $PERSON1" 
+echo "$PERSON1:: ok $PERSON2"
