54936ab61497fc2430e25a702c5e4bd23ac3efc9
subbu <subbunandyala48@gmail.com>
2025-10-27 09:07:13 +0530

    hello world

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -8,4 +8,4 @@ TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP" 
 sum=$(($NUMBER1 + $NUMBER2))
 
-echo "sum of $NUMBER1 and $NUMBER2 is: $sum" 
+echo "sum of $NUMBER1 and $NUMBER2 is: $SUM" 
