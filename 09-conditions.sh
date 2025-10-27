8afcab44566475ef2c3586469d7a5f5b51fe870d
subbu <subbunandyala48@gmail.com>
2025-10-27 10:53:30 +0530

    hello world

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+NUMBER=$1 
+# -gt greater than 
+# -lt less than 
+# -eq eaqual 
+# -ne not equal
+
+if [$NUMBER -lt 10] {
+    echo "given number $NUMBER is less than 10" 
+else 
+   echo " given number $NUMBER is not less than 10" 
+fi    
