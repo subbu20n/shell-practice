26024229d7e45b3f287444de723ee75cd657f333
subbu <subbunandyala48@gmail.com>
2025-10-27 10:39:29 +0530

    hello world

---
+++ 08-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "all  variables passed to the script: $@" 
+echo "number of variables passed to the script: $#" 
+echo "script name: $0" 
+echo "present working directory: $PWD" 
+echo "home directory of the user who is running the script: $HOME" 
+echo "which user running the script: $USER" 
+echo "PID of the current script: $$" 
+sleep 10$
+echo "PID of last command running in background: $!" 
+echo "exit status of last command: $?" 
