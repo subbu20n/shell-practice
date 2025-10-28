34298c97541e16ada005bf84a01341338cd19e68
subbu <subbunandyala48@gmail.com>
2025-10-28 12:25:10 +0530

    hello world

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -55,6 +55,6 @@ done <<< $FILES_TO_DELETE
 echo -e "script executed successfully" | tee -a $LOG_FILE 
 
 END_TIME=$(date +%s) 
-TOTAL-TIME=$(($END_TIME - $START_TIME)) 
+TOTAL_TIME=$(($END_TIME - $START_TIME)) 
 
 echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE  
