a52e9022ecceeea97628ff1f658336c22672d88f
subbu <subbunandyala48@gmail.com>
2025-10-28 17:13:24 +0530

    hello world

--- 23-memory-usage.sh
+++ 23-memory-usage.sh
@@ -5,7 +5,7 @@ total_memory=$(echo "$memory_info" | awk '{print $2}')
 used_memory=$(echo "$memory_info" | awk '{print $3}')
 
 if [[ "$total_memory" -gt 0 ]]; then
-    memory_percentage=$(echo "scale=2; ($used_memory * 100) / $total_memory" | bc)
+    memory_percentage=$(echo "scale=2; ($used_memory * 100) / $total_memory" | )
     echo "Memory Usage: ${used_memory}MB / ${total_memory}MB (${memory_percentage}%)"
 else
     echo "Error: Total memory is zero or not available."
