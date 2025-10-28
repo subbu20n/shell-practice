8491accf92913f10ecae1b945e8267c3c587d9b3
subbu <subbunandyala48@gmail.com>
2025-10-28 11:24:56 +0530

    hello WORLD

--- 19-while-loop.sh
+++ 19-while-loop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-a=0 
-while [ $a -lt 10 ] 
-do
-  echo $a 
-  a=`expr $a + 1` 
-done 
+# a=0 
+# while [ $a -lt 10 ] 
+# do
+#   echo $a 
+#   a=`expr $a + 1` 
+# done 
 
 while IFS= read -r line 
 do 
