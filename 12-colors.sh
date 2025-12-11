7d81096015c13b3a462ebb298bc0430a5451211f
subbu <subbunandyala48@gmail.com>
2025-12-11 20:39:45 +0530

    hello world

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
 
-echo -e "\e[30m hello subbu" 
-echo -e "\e[31m hello subbu" 
+echo -e "\e[31m hello world" 
 echo -e "\e[32m hello subbu" 
-echo -e "\e[33m hello subbu" 
-echo -e "\e[35m hello subbu" 
-echo -e "\e[36m hello subbu" 
-echo -e "\e[37m hello subbu" 
-echo -e "\e[0m hello subbu" 
+echo -e "\e[33m hello india" 
+echo -e "\e[34m hello arun " 
+echo -e "\e[35m hello hari" 
+echo -e "\e[36m hello pavan" 
+echo -e "\e[37m hello shiva" 
+echo -e "\e[0m hello amma" 
 
-echo -e "\e[32m hello subbu" 
-echo "hello supriya" 
+
+echo -e "\e[33m hello subbu" 
+echo "hello world" 
