f407958f2bebc03c72d26a9e2e1c125344d92330
subbu <subbunandyala48@gmail.com>
2025-12-11 20:06:19 +0530

    hello world

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,11 +5,11 @@ NUMBER=$1
 # -gt greater than 
 # -lt less than 
 # -eq equal 
-# -ne not equal to 
+# -ne not equal 
 
-if [ $NUMBER -lt 0 ]:
+if [ $NUMBER -lt 10 ] 
 then 
   echo "Given number $NUMBER is less than 10" 
-else
-  echo "Given number $NUMBER is not lees than 10" 
-fi 
+else 
+  echo "Given number $NUMBER is not less than 10" 
+fi      
--- 10-install-command.sh
+++ 10-install-command.sh
@@ -1,27 +1,28 @@
 #!/bin/bash 
 
 USERID=$(id -u) 
- 
+
 if [ $USERID -ne 0 ] 
 then 
-  echo "ERROR:: please run this script with root access" 
-  exit 1 
+   echo "ERROR:: please run this script with root access" 
+   exit 1 
 else 
   echo "you are running with root access" 
-fi
+fi 
 
-dnf list installed mysql 
+dnf list installed nginx 
 
 if [ $? -ne 0 ] 
 then 
-   echo "MYSQL is not installed ... going to install" 
-   dnf install mysql -y 
+   echo "nginx is not installed ... going to installed" 
+   dnf install nginx -y 
    if [ $? -eq 0 ] 
    then 
-      echo "Installing MYSQL is ... SUCCESS" 
+      echo "Installing nginx is .. success"
    else 
-      echo "Installing MYSQL is ... FAILURE" 
+      echo "Installing nginx is .. failure" 
+      exit1 
    fi 
 else 
-  echo "MYSQL is already installed ... nothing to do" 
-fi 
+   echo "nginx is already installed ... nothing to do" 
+fi             
