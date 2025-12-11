99709dcdef6b8d00062339f9d31995c9d881e28b
subbu <subbunandyala48@gmail.com>
2025-12-11 20:31:20 +0530

    hello world

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,49 +4,48 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ] 
 then 
-  echo "ERROR:: please run the script with root access" 
-  exit 1 
+  echo " ERROR:: Please run thi script with root access'" 
+  exit1 
 else 
   echo "you are running with root access" 
 fi 
 
-#validate function takes input as exit status , what command they tried to install 
-validate(){
-    if [ $1 -eq 0 ]
-    then 
-       echo "Installing $2 is ... SUCCESS" 
-    else 
-       echo "Installing $2 is ... FAILURE" 
-       exit1 
-    fi       
-}
+# validation function takes input as exit status what command they tried to install 
+if [ $? -eq 0 ] 
+then 
+   echo "Installing $2 is ... success" 
+else 
+   echo "Installing $2 is ... failure" 
+   exit1 
+fi 
 
 dnf list installed mysql 
+if [ $? -ne 0 ]
+then 
+   echo "mysql is not installed ... going to install" 
+   dnf install mysql -y 
+   VALIDATE $? "MYSQL" 
+else 
+  echo "mysql is already installed ... nothing to do" 
+fi 
+
+dnf list installed nginx 
 if [ $? -ne 0 ] 
 then 
-  echo "Mysql is not installed ... going to install" 
-  dnf install mysql -y 
-  validate $? "MYSQL" 
-else 
-  echo "Mysql is already installed ... nothing to do" 
+   echo "nginx is ot installed ... going to install" 
+   dnf install nginx -y 
+   VALIDATE $? "NGINX" 
+else
+   echo "nginx is already installed ... nothing to do" 
 fi 
 
 dnf list installed python3 
-if [ $? -ne 0 ] 
+if [ $? -ne 0 ]
 then 
-  echo "python3 is not installed ... going to install" 
-  dnf install python3 -y 
-  validate $? "PYTHON3" 
+   echo " python3 is not installed ...going to install" 
+   dnf install python3 -y 
+   VALIDATE $? "PYTHON3" 
 else 
-  echo "Python3 is already installed ... nothing to do" 
-fi 
-
-dnf list installed ngins 
-if [ $? -ne 0 ] 
-then 
-  echo "nginx is not installed ... going to install" 
-  dnf install nginx -y 
-  validate $? "NGINX" 
-else 
-  echo "nginx is already installed ... nothing to do" 
-fi     
+   echo "python3 is already installed ... nothing to do" 
+fi         
+  
