2f1d47f152660fd381424cd701759ec1322eb9fa
subbu <subbunandyala48@gmail.com>
2025-12-11 19:57:55 +0530

    hello world

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,11 +5,11 @@ NUMBER=$1
 # -gt greater than 
 # -lt less than 
 # -eq equal 
-# -ne not equal 
+# -ne not equal to 
 
-if [ $NUMBER -lt 10 ] 
+if [$NUMBER -lt 0] 
 then 
   echo "Given number $NUMBER is less than 10" 
-else 
-  echo "Given number $NUMBER is not less than 10" 
-fi     
+else
+  echo "Given number $NUMBER is not lees than 10" 
+fi 
