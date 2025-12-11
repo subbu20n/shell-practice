fefd8f8aa7979ebeb95f887120875688a9d41182
subbu <subbunandyala48@gmail.com>
2025-12-11 20:07:21 +0530

    hello world

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -1,28 +1,27 @@
 #!/bin/bash 
 
 USERID=$(id -u) 
-
+ 
 if [ $USERID -ne 0 ] 
 then 
-   echo "ERROR:: please run this script with root access" 
-   exit 1 
+  echo "ERROR:: please run this script with root access" 
+  exit 1 
 else 
   echo "you are running with root access" 
-fi 
+fi
 
-dnf list installed nginx 
+dnf list installed mysql 
 
 if [ $? -ne 0 ] 
 then 
-   echo "nginx is not installed ... going to installed" 
-   dnf install nginx -y 
+   echo "MYSQL is not installed ... going to install" 
+   dnf install mysql -y 
    if [ $? -eq 0 ] 
    then 
-      echo "Installing nginx is .. success"
+      echo "Installing MYSQL is ... SUCCESS" 
    else 
-      echo "Installing nginx is .. failure" 
-      exit1 
+      echo "Installing MYSQL is ... FAILURE" 
    fi 
-else 
-   echo "nginx is already installed ... nothing to do" 
-fi             
+else  
+  echo "MYSQL is already installed ... nothing to do" 
+fi 
