0bbed8eb0908cbdcc69c1d7b3758cfcb858d60f0
subbu <subbunandyala48@gmail.com>
2025-12-11 19:45:47 +0530

    hello world

--- 07-array.sh
+++ 07-array.sh
@@ -1,5 +1,14 @@
 #!/bin/bash 
 
-MOVIES=("subbu" "siva") 
+MOVIES=("subbu" "arun" "legend" "simha") 
 
-echo "first movie: ${MOVIES[0]}"
+echo "first movie: ${MOVIES[0]}" 
+echo "first movie: ${MOVIES[1]}" 
+echo "first movie: ${MOVIES[2]}" 
+echo "first movie: ${MOVIES[3]}" 
+echo "first movie: ${MOVIES[@]}" 
+
+for i in 0 3 
+do 
+  echo "movie: ${MOVIES[$i]}" 
+done   
