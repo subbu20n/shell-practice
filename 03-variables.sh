973928a48f211e5c19ecd2df68326e45114888d4
subbu <subbunandyala48@gmail.com>
2025-12-11 19:00:50 +0530

    hello world

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-PERSON1=SUBBU
-PERSON2=ARUN 
+2PERSON1=SUBBU
+1PERSON2=ARUN 
 
 echo "$PERSON1:: hi $PERSON2, how are you" 
 echo "$PERSON2:: Hlo $PERSON1, i am fine, what about you?" 
