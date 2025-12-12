b4a0d59d9c4a5bd53e1f5654c4f49e0c9e6bc531
subbu <subbunandyala48@gmail.com>
2025-12-12 16:28:03 +0530

    hello world

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-SOURCE_DIR="home/ec2-user/app-logs" 
+SOURCE_DIR=/home/ec2-user/app-logs
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14) 
 while IFS= read -r filepath 
